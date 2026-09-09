ad3771451c700ea5949b7b37b67cab5df5c4d6f5
Ny Aina Fanantenana Rajaonarivelo <nyrajaon@c1r6s13.42antananarivo.mg>
2026-09-09 12:09:55 +0300

    feat(header): add args, coder, dongle struct

--- codexion.h
+++ codexion.h
@@ -1,10 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   codexion.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nyrajaon <nyrajaon@student.42antananari    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/09 10:03:13 by nyrajaon          #+#    #+#             */
+/*   Updated: 2026/09/09 12:03:07 by nyrajaon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef CODEXION_H
 # define CODEXION_H
 
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <pthread.h>
-#include <sys/time.h>
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+# include <pthread.h>
+# include <sys/time.h>
+
+typedef struct s_args
+{
+	int		number_of_coders;
+	long	time_to_burnout;
+	long	time_to_compile;
+	long	time_to_debug;
+	long	time_to_refactor;
+	long	number_of_compiles_required;
+	long	dongle_cooldown;
+	char	*scheduler;
+}	t_args;
+
+typedef struct s_coder
+{
+	int	id;
+}	t_coder;
+
+typedef struct s_dongle
+{
+	int	id;
+}	t_dongle;
 
 #endif
--- main.c
+++ main.c
@@ -1,18 +1,27 @@
-#include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nyrajaon <nyrajaon@student.42antananari    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/09 09:57:59 by nyrajaon          #+#    #+#             */
+/*   Updated: 2026/09/09 10:58:56 by nyrajaon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#include "codexion.h"
 
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
-	if (ac != 8)
+	int	i;
+
+	i = 0;
+	if (ac >= 9)
 	{
 		printf("Error: Invalid number of arguments\n");
 		return (1);
 	}
-
-	int i;
-	i = 0;
-	// while()
-	av[1] = "d";
-	printf("Hello");
+	printf("%d\n", atoi(av[1]));
 	return (0);
 }
